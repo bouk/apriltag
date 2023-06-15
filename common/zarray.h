0ec3dbb12c5bb4b41f6195f3c761f4af8d1b7eb3
Bouke van der Bijl <i@bou.ke>
2023-06-15 14:34:10 +0200

    Speed up cluster merge by batch copying arrays

--- common/zarray.h
+++ common/zarray.h
@@ -437,7 +437,28 @@ static inline int zarray_index_of(const zarray_t *za, const void *p)
     return -1;
 }
 
+/**
+ * Add elements from start up to and excluding end from 'source' into 'dest'.
+ * el_size must be the same for both lists
+ **/
+static inline void zarray_add_range(zarray_t *dest, const zarray_t *source, int start, int end)
+{
+    assert(dest->el_sz == source->el_sz);
+    assert(dest != NULL);
+    assert(source != NULL);
+    assert(start >= 0);
+    assert(end <= source->size);
+    if (start == end) {
+        return;
+    }
+    assert(start < end);
 
+    int count = end - start;
+    zarray_ensure_capacity(dest, dest->size + count);
+
+    memcpy(&dest->data[dest->size*dest->el_sz], &source->data[source->el_sz*start], dest->el_sz*count);
+    dest->size += count;
+}
 
 /**
  * Add all elements from 'source' into 'dest'. el_size must be the same
@@ -445,18 +466,7 @@ static inline int zarray_index_of(const zarray_t *za, const void *p)
  **/
 static inline void zarray_add_all(zarray_t * dest, const zarray_t * source)
 {
-    assert(dest->el_sz == source->el_sz);
-
-    // Don't allocate on stack because el_sz could be larger than ~8 MB
-    // stack size
-    char *tmp = (char*)calloc(1, dest->el_sz);
-
-    for (int i = 0; i < zarray_size(source); i++) {
-        zarray_get(source, i, tmp);
-        zarray_add(dest, tmp);
-   }
-
-    free(tmp);
+    zarray_add_range(dest, source, 0, source->size);
 }
 
 #ifdef __cplusplus
